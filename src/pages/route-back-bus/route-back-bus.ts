f404d13d2c97485cdf7e8352696ae552bb5cf336
Diego Leonardo Vivanco Moreira <leox_vm@hotmail.com>
2017-06-06 17:41:51 +0200

    EMT-palma

--- src/pages/route-back-bus/route-back-bus.ts
+++ src/pages/route-back-bus/route-back-bus.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-declare let google;
+import { ParadasMapa } from '../../providers/paradas-mapa';
+
 /**
  * Generated class for the RouteBackBus page.
  *
@@ -14,23 +15,28 @@ declare let google;
 })
 export class RouteBackBus {
 
-  @ViewChild('mapBack') mapElement;
   mapBack;
   showMap: any;
   hideFabMap: any;
   hideFapRoute: any;
-  rutaSeleccionada;
-  frecuenciasBack;
+  routeSelected;
+  frequenciesBack;
+  routeMapBack: any;
+  paradasMapa: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.rutaSeleccionada = navParams.data;
-    this.frecuenciasBack = this.rutaSeleccionada.frecuencias;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public paradasMapaService: ParadasMapa) {
+
+    this.routeSelected = navParams.data;
+    this.frequenciesBack = this.routeSelected.frecuencias;
+    this.routeMapBack = this.routeSelected.recorridoIda;
+    this.paradasMapa = this.paradasMapaService.loadParadasMapa()[0];
     this.showMap = false;
     this.hideFabMap = false;
     this.hideFapRoute = true;
-    console.log(this.frecuenciasBack);
   }
-
 
   showMapBack(){
     this.showMap = true;
@@ -39,27 +45,49 @@ export class RouteBackBus {
     setTimeout(() => {
       this.mapRouteBack();
     }, 300);
-
   }
 
   showRouteBack(){
     this.showMap = false;
     this.hideFabMap = false;
     this.hideFapRoute = true;
-
   }
 
    mapRouteBack(){
-    let latLngTwo = new google.maps.LatLng(39.5830906, 2.6540206);
+     this.mapBack = L.map('mapBack').setView([39.5748641, 2.6449896], 13);
 
-   let mapOptionsTwo = {
-      center: latLngTwo,
-      zoom: 12,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+     L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}.png?access_token={accessToken}', {
+       zoom: 13,
+       accessToken: 'pk.eyJ1IjoiZGx2aXZhbmNvIiwiYSI6ImNqMzBjY3ZpcTAwMWcycXBnN251b3M0Z2IifQ.qhYk3raWsVyuhbMvr1B4LA'
+     }).addTo(this.mapBack);
 
-   this.mapBack = new google.maps.Map(this.mapElement.nativeElement, mapOptionsTwo);
-
+     L.polyline(this.routeMapBack).addTo(this.mapBack);
+     let routeBackCoordList = this.getRouteBackCoordsList();
+     this.showMarkersBack(routeBackCoordList);
  }
+
+  getRouteBackCoordsList(){
+    let result = this.routeSelected.vuelta
+      .map(String)
+      .map((stopTmp: string) => this.paradasMapa[stopTmp]);
+
+    return result;
+  }
+
+  showMarkersBack(routeBackCoordList) {
+    let numberStopList = this.routeSelected.vuelta;
+    for (let key in routeBackCoordList) {
+      if (routeBackCoordList[key][0] !== null) {
+        let busIcon = new L.DivIcon({
+          className: 'myDivIcon',
+          html: '<img class="myDivImage" src="assets/img/bus2.png"/>' +
+          '<div class="myDivNumber">' + numberStopList[key] + '</div>'
+        });
+
+        L.marker([routeBackCoordList[key][0], routeBackCoordList[key][1]], {icon: busIcon})
+          .addTo(this.mapBack);
+      }
+    }
+  }
 
 }
--- src/pages/route-forward-bus/route-forward-bus.ts
+++ src/pages/route-forward-bus/route-forward-bus.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
-declare let google;
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ParadasMapa } from '../../providers/paradas-mapa';
+
 /**
  * Generated class for the RouteForwardBus page.
  *
@@ -14,50 +15,79 @@ declare let google;
 })
 export class RouteForwardBus {
 
-  @ViewChild('mapForward') mapElement;
-  map;
+  mapForward;
   showMap: any;
   hideFabMap: any;
   hideFapRoute: any;
-  rutaSeleccionada;
-  frecuenciasForward;
+  routeSelected;
+  frequenciesForward;
+  routeMapForward: any;
+  paradasMapa: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events) {
-    this.rutaSeleccionada = navParams.data;
-    this.frecuenciasForward = this.rutaSeleccionada.frecuencias;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public paradasMapaService: ParadasMapa) {
+
+    this.routeSelected = navParams.data;
+    this.frequenciesForward = this.routeSelected.frecuencias;
+    this.routeMapForward = this.routeSelected.recorridoIda;
+    this.paradasMapa = this.paradasMapaService.loadParadasMapa()[0];
     this.showMap = false;
     this.hideFabMap = false;
     this.hideFapRoute = true;
-    console.log(this.frecuenciasForward);
-
   }
 
-showMapForward(){
-    this.showMap = true;
-    this.hideFabMap = true;
-    this.hideFapRoute = false;
-    setTimeout(() => {
-      this.mapRouteForward();
-    }, 300);
-  }
-
- showRouteForward(){
-    this.showMap = false;
-    this.hideFabMap = false;
-    this.hideFapRoute = true;
-
- }
-
- mapRouteForward(){
-    let latLng = new google.maps.LatLng(39.5830906, 2.6540206);
-
-   let mapOptions = {
-      center: latLng,
-      zoom: 12,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+  showMapForward(){
+      this.showMap = true;
+      this.hideFabMap = true;
+      this.hideFapRoute = false;
+      setTimeout(() => {
+        this.mapRouteForward();
+      }, 300);
     }
 
-   this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+   showRouteForward(){
+      this.showMap = false;
+      this.hideFabMap = false;
+      this.hideFapRoute = true;
+   }
 
- }
+   mapRouteForward(){
+     this.mapForward = L.map('mapForward').setView([39.5748641, 2.6449896], 13);
+
+     L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.streets-basic/{z}/{x}/{y}.png?access_token={accessToken}', {
+       zoom: 13,
+       accessToken: 'pk.eyJ1IjoiZGx2aXZhbmNvIiwiYSI6ImNqMzBjY3ZpcTAwMWcycXBnN251b3M0Z2IifQ.qhYk3raWsVyuhbMvr1B4LA'
+     }).addTo(this.mapForward);
+
+     L.polyline(this.routeMapForward).addTo(this.mapForward);
+     let routeForwardCoordList = this.getRouteForwardCoordsList();
+     this.showMarkersForward(routeForwardCoordList);
+   }
+
+   getRouteForwardCoordsList(){
+    let result = this.routeSelected.ida
+       .map(String)
+       .map((stopTmp: string) => this.paradasMapa[stopTmp]);
+
+    return result;
+   }
+
+   showMarkersForward(routeForwardCoordList) {
+     let numberStopList = this.routeSelected.ida;
+     for (let key in routeForwardCoordList) {
+       if (routeForwardCoordList[key][0] !== null) {
+         let busIcon = new L.DivIcon({
+           className: 'myDivIcon',
+           html: '<img class="myDivImage" src="assets/img/bus2.png"/>' +
+           '<div class="myDivNumber">' + numberStopList[key] + '</div>'
+         });
+
+         L.marker([routeForwardCoordList[key][0], routeForwardCoordList[key][1]], {icon: busIcon})
+           .addTo(this.mapForward);
+       }
+     }
+   }
+
 }
